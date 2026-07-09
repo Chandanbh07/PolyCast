92af851c5c10c623d34072a6f62920396f52f4a6
Chandanagouda <chandanhiregoudra@gmail.com>
2026-07-09 12:23:51 +0530

    Debug database_URL

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,22 @@
+// import { PrismaClient } from "./generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+
+// export const prisma = new PrismaClient({
+//   adapter,
+// });
+
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+
+console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
+console.log(
+  "DATABASE_URL starts with:",
+  process.env.DATABASE_URL?.split("@")[0]
+);
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
